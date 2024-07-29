74a1beafc8417100ae48544da09e9c7f784279d5
shiroko <thothctm@gmail.com>
2024-07-28 22:01:44 -0400

    fix vite import

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from "mdsvex";
 import adapter from "@sveltejs/adapter-netlify";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,9 +9,6 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    paths: {
-        base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    }
   },
 
   preprocess: [vitePreprocess(), mdsvex()],
