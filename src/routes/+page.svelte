b804ce84f920ddc495fc131e0d92562e813f935f
shiroko <thothctm@gmail.com>
2024-08-11 20:05:37 -0400

    Update landing page and added gallery links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,31 @@
 <script>
     import "./essence/markdown.css"
 </script>
+
 <div style="max-width: 60%; margin: auto; padding-top: 1rem;">
     <h1>Leatrux Archives</h1>
     <ul>
-        <li><h2><a href="alchemy">Alchemy</a></h2></li>
-        <li><h2><a href="shop">Merchants</a></h2></li>
-        <li><h2><a href="essence">Essence</a></h2></li>
-        <li><h2><a href="continents/phudara">Phudara Tribes</a></h2></li>
+        <li>
+            <h2>System Rules</h2>
+            <ul>
+                <li><h3><a href="alchemy">Alchemy</a></h3></li>
+                <li><h3><a href="essence">Essences</a></h3></li>
+            </ul>
+        </li>
+        <li>
+            <h2>Campaign Resources</h2>
+            <ul>
+                <li><h3><a href="continents/phudara">Phudara Tribes</a></h3></li>
+                <li><h3><a href="shop">Merchants</a></h3></li>
+            </ul>
+        </li>
+        <li>
+            <h2>Campaign Gallery</h2>
+            <ul>
+                <li><h3><a href="maps">Maps</a></h3></li>
+                <li><h3><a href="http://dintdt.c1.biz/">Sprites</a></h3></li>
+            </ul>
+        </li>
     </ul>
 </div>
 
---
+++ src/routes/maps/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../essence/markdown.css"
+</script>
+
+<div style="max-width: 60%; margin: auto; padding-top: 1rem;">
+    <h1>Phudara</h1>
+    <h2>The Isle of Whispers</h2>
+    <img src="https://cdn.discordapp.com/attachments/527269598857396236/1267682457881153628/Isle_of_whispers-min.jpg?ex=66ba278a&is=66b8d60a&hm=3cdeeca66738e0bd8359ae1ad0f063b7906defaf5bd879b33fddc4fe5b0dd8cf&">
+</div>
+
+
